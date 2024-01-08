be72efc0a896376805d10aeb6cb6d41dc67995a8
romanedorrel <romanedorrel@gmail.com>
2024-01-08 02:54:58 -0500

    exercise 4

--- Backend/helloworld.js
+++ Backend/helloworld.js
@@ -1,1 +1,21 @@
 console.log('Hello World')
+function sum(a, b){
+    console.log(a + b)
+}
+function difference(c, d){
+    console.log(c - d)
+}
+function product(e, f){
+    console.log(e * f)
+}
+function divide(g, h){
+    console.log(g / h)
+}
+function name(name){
+    console.log("Hello", name)
+}
+name("John");
+sum(3,8);
+difference(10,7);
+product(2, 4);
+divide(9, 3);
