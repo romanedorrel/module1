ed7a33d94cda201553a98579adf55c4158d73e7c
romanedorrel <romanedorrel@gmail.com>
2024-01-10 23:05:19 -0500

    exercise 6

--- test.js
+++ test.js
@@ -3,5 +3,15 @@ name.unshift('Joe');
 name.pop();
 console.log(name);
 const book = {
-    
+    "Title":"Story of now and never",
+    "Description":"Fiction",
+    "Author":"John Nobody",
+    "Number_of_pages":356    
 }
+console.log(book.Title);
+console.log(book.Description);
+console.log(book.Author);
+console.log(book.Number_of_pages);
+console.log(book);
+book.Description ="Biography";
+console.log(book);
